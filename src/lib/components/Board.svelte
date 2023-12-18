5472259597d2095d9be3dbd56bacdc019dc953db
Justin Stitt <jstitt007@gmail.com>
2023-12-17 18:59:53 -0800

    un-ignore lib

---
+++ src/lib/components/Board.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { type Piece } from "scouts-wasm";
+  const rows = 10;
+  const cols = 8;
+  const arr = Array(rows * cols).fill("A");
+
+  const pieces: Piece[] = [
+    { kind: "scout", player: 1, position: "2,3", returning: false },
+  ];
+
+  const pos_to_idx = (x, y) => {
+    return y * rows + x;
+  };
+
+  pieces.forEach((piece) => {});
+</script>
+
+<div class="main">
+  {#each arr as item}
+    <div class="cell">{item}</div>
+  {/each}
+</div>
+
+<style>
+  .main {
+    display: grid;
+    grid-template-rows: repeat(10, 30px);
+    grid-template-columns: repeat(8, 30px);
+  }
+
+  .cell {
+    width: 40;
+    aspect-ratio: 1;
+    background-color: yellow;
+    border: 2px dotted black;
+    color: black;
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import './app.css'
 import App from './App.svelte'
 
+
+const app_target = document.getElementById("app");
+if (!app_target) {
+  throw new Error("Could not find app target in DOM.");
+}
+
 const app = new App({
-  target: document.getElementById('app'),
+  target: app_target,
 })
 
 export default app
