174bbbc7f431b4f4a729cfb9204a2ad78914bd5f
diamondburned <diamond@arikawa-hi.me>
2023-12-17 20:18:55 -0800

    Set up "#lib/*" for absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   clearScreen: false,
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      "#lib": path.resolve(__dirname, "./src/lib"),
+    },
+  },
 });
